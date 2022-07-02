33336e0de955282b444b0cde804ad4cf366571eb
Mohit Sharma <iamxguy352@gmail.com>
2022-07-02 23:24:12 +0530

    Refactored the code using classes

--- script1.js
+++ script1.js
@@ -11,36 +11,62 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
-//Function to get the current location
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { longitude } = position.coords;
-      const { latitude } = position.coords;
+//Refactoring the code using Class
 
-      const coords = [latitude, longitude];
+class App {
+  #map;
+  #mapEvent;
 
-      map = L.map('map').setView(coords, 13);
+  constructor() {
+    this.__getPosition();
+    form.addEventListener('submit', this.__newWorkout.bind(this));
+    //for changing the input types for cycling and running
+    inputType.addEventListener('change', this.__toggleElevationField);
+  }
 
-      //LeafLet Library for Map
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  //This method gives the current position of the person
+  __getPosition() {
+    //Function to get the current location
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this.__loadMap.bind(this),
+        function () {
+          alert(`Not able to find your current location.`);
+        }
+      );
+  }
 
-      //Map Marker Implementation
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    function () {
-      alert(`Not able to find your current location.`);
-    }
-  );
+  //This Method Load the map into the Website
+  __loadMap(position) {
+    const { longitude } = position.coords;
+    const { latitude } = position.coords;
 
-  form.addEventListener('submit', function (e) {
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, 15);
+
+    //LeafLet Library for Map
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    //Map Marker Implementation
+    this.#map.on('click', this.__showForm.bind(this));
+  }
+
+  __showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  __toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  __newWorkout(e) {
     e.preventDefault();
 
     //clearing input fields
@@ -52,10 +78,10 @@ if (navigator.geolocation) {
         '';
 
     //For Displaying Marker
-    const { lat, lng } = mapEvent.latlng;
+    const { lat, lng } = this.#mapEvent.latlng;
 
     L.marker([lat, lng])
-      .addTo(map)
+      .addTo(this.#map)
       .bindPopup(
         L.popup({
           maxWidth: 250,
@@ -67,12 +93,7 @@ if (navigator.geolocation) {
       )
       .setPopupContent('Workout')
       .openPopup();
-      
-  });
+  }
 }
 
-//for changing the input types for cycling and running
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
